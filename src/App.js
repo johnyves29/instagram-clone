611e6b838c9405cc771ffb642878c2927629ce9a
johnyvesmarin29 <johnyvesmarin@gmail.com>
2021-04-06 21:44:17 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,199 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import { auth, db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  //modal in center
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  //modal styles
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [posts, setPosts] = useState([]); //for posts
+  const [open, setOpen] = useState(false); //for signup
+  const [openSignIn, setOpenSignIn] = useState(false); //for login
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has login...
+        console.log(authUser);
+        setUser(authUser);
+
+        if (authUser.displayName) {
+          //dont update username
+        } else {
+          //if we just createrd someone..
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      } else {
+        //user has logout...
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //perform some cleanup actions..
+      unsubscribe();
+    };
+  }, [user, username]);
+
+  // useEffect runs piece of code base on specific condition
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      //everytime post is added, this codes fires..
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <form className="app__signup">
+              <img
+                className="app__headerImage"
+                src="https://assets.website-files.com/5c75b94c8dd1ae50d3b9294b/5d48831280adb734a5db5620_hukglfkfklk%3B.png"
+                alt=""
+              />
+              <Input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signUp}>Sign Up</Button>
+            </form>
+          </center>
+        </div>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <form className="app__signup">
+              <img
+                className="app__headerImage"
+                src="https://assets.website-files.com/5c75b94c8dd1ae50d3b9294b/5d48831280adb734a5db5620_hukglfkfklk%3B.png"
+                alt=""
+              />
+              <Input
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signIn}>Login</Button>
+            </form>
+          </center>
+        </div>
+      </Modal>
+
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://assets.website-files.com/5c75b94c8dd1ae50d3b9294b/5d48831280adb734a5db5620_hukglfkfklk%3B.png"
+          alt=""
+        />
+      </div>
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Login</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        </div>
+      )}
+
+      <h1>test Emmet!</h1>
+
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+  const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDEsLtxH2NORrJZaW9sP5HRq1RwBUaeu40",
+    authDomain: "instagram-clone-react-14ed9.firebaseapp.com",
+    projectId: "instagram-clone-react-14ed9",
+    storageBucket: "instagram-clone-react-14ed9.appspot.com",
+    messagingSenderId: "532771977711",
+    appId: "1:532771977711:web:1108b62f92999b3a4d5e1a",
+    measurementId: "G-B1YMQMTJ46"
+  });
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+
+  export { db, auth, storage };
